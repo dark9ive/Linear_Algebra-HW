088ee834f6e29550ecc19e0fa3cd16efd2cd67cc
Est <asdfg3655@yahoo.com.tw>
2020-03-12 18:39:19 +0800

    upload on Mar 12, 2020

---
+++ week02/homework02.py
@@ -0,0 +1,12 @@
+import numpy as npy
+x = npy.array([1, 2, 3])
+print(x)
+x
+y = npy.array([[1], [2], [3]])
+ans = npy.dot(x, y)
+ans
+array([14])
+y = npy.array([1, 2, 3])
+ans = npy.dot(x, y)
+ans
+14
