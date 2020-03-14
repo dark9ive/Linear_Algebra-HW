a53fa11c38b5ced4feb5b5f6fcbf6499ae8fbfa9
Est <asdfg3655@yahoo.com.tw>
2020-03-15 04:53:27 +0800

    upload on Mar 15, 2020

--- week02/homework02.py
+++ week02/homework02.py
@@ -1,9 +1,10 @@
-import numpy as npy             #   import numpy
-x = npy.array([1, 2, 3])        #   assign x as a 1×3 array
-y = npy.array([[1], [2], [3]])  #   assign y as a 3×1 array
-ans = npy.dot(x, y)             #   dot operation
-print(ans)                      #   print the answer of "ans"
-y = npy.array([1, 2, 3])        #   assign y as a 1×3 array
-ans = npy.dot(x, y)             #   dot operation
-print(ans)                      #   print the answer of "ans"
-exit()                          #   exit
+import numpy as npy                     #   import numpy
+x = npy.array([[1, 2, 3], [1, 2, 3]])   #   assign x as a 2×3 array
+y = npy.array([[1, 2], [1, 2], [1, 2]]) #   assign y as a 3×2 array
+ans = npy.dot(x, y)                     #   dot operation
+print(ans)                              #   print the answer of "ans"
+x = npy.array([3, 2, 1])                #   assign y as a 1×3 vector
+y = npy.array([1, 2, 3])                #   assign y as a 1×3 vector
+ans = npy.dot(x, y)                     #   dot operation
+print(ans)                              #   print the answer of "ans"
+exit()                                  #   exit
