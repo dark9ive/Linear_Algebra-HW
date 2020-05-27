950ad28af69c0769ca63d564f312635037a70cb9
Est <asdfg3655@yahoo.com.tw>
2020-05-27 23:27:06 +0800

    upload on May 27, 2020

---
+++ week11/programmingHW/README.md
@@ -0,0 +1,42 @@
+# ProgrammingHW06
+
+Linear Algebra Programming HW 06
+
+## usage
+
+codes [here](https://github.com/dark9ive/Linear_Algebra-HW/blob/master/week09/programmingHW/code.py).
+
+```
+python3 code.py
+```
+
+I recommend you to run these codes on a ___jupyter notebook___ or anything similar since terminal enviornments can't show the charts.  
+
+## principle
+
+### Part 1
+
+So this is how my codes works:  
+
+First the user will insert a range for a rectangle, then I will fill the region with 30x30 points separated.
+
+After that, the code will draw each points on a chart, which ranged from x=(-1.2\*max, 1.2\*max), y=(-1.2\*max, 1.2\*max) for max=max(eachX, eachY).
+
+This is all of Part 1.  
+
+### Part 2
+
+In part 2, the user is asked to insert the degree(s) to rotate, and the x-direction and y-direction transform.
+
+The transform matrix is calcucate as follows:
+
+1. First make a 3x3 identity matrix.
+2. Multiply(npy.dot) the matrix with rotate-matrix, which is [[cos(theta), -sin(theta), 0], [sin(theta), cos(theta), 0], [0, 0, 1]]
+3. Multiply(npy.dot) the matrix with transform-matrix, which is [[1, 0, deltaX], [0, 1, deltaY], [0, 0, 1]]
+
+then multiply every points with the 3x3 transform matrix and draw them on a new chart.
+
+Finally, my code prints out the transform matrix for the user to verify.
+
+## Example
+<img src=https://github.com/dark9ive/Linear_Algebra-HW/blob/master/week09/programmingHW/pics/Myexample.png >.
---
+++ week11/programmingHW/code.py
@@ -0,0 +1,77 @@
+import numpy as npy
+import matplotlib.pyplot as plt
+import math
+
+def transform(a, b):            #    returns a 3x3 matrix  
+    returnval = npy.zeros(shape=(3, 3))
+    returnval = [[1, 0, a], [0, 1, b], [0, 0, 1]]
+    return returnval
+
+def rotate(theta):
+    theta = theta/360*math.pi*2
+    returnval = npy.zeros(shape=(3, 3))
+    returnval = [[math.cos(theta), -math.sin(theta), 0], [math.sin(theta), math.cos(theta), 0], [0, 0, 1]]
+    return returnval
+
+if __name__ == '__main__':
+    buf = 0
+    while(buf == 0):
+        deltax = float(input("Please insert deltax: "))
+        deltay = float(input("Please insert deltay: "))
+        buf = math.pow(deltax, 2)+math.pow(deltay, 2)
+    buf = math.pow(buf, 0.5)
+    deltax /= buf
+    deltay /= buf
+    x0 = float(input("Please insert x0: "))
+    y0 = float(input("Please insert y0: "))
+    
+    x = npy.linspace(x0-10*deltax, x0+10*deltax, 100)
+    y = npy.linspace(y0-10*deltay, y0+10*deltay, 100)
+    max_ = 0
+    
+    points = []            #    homogeneous coordinates array
+    
+    for i in range(0, 100):
+        plt.scatter(x[i], y[i], 20, 'r')
+        if(abs(x[i]) > max_):
+            max_ = abs(x[i])
+        if(abs(y[i]) > max_):
+            max_ = abs(y[i])
+        points.append([x[i], y[i], 1])
+    plt.xlim(-max_*1.2, max_*1.2)        #    re-range the chart
+    plt.ylim(-max_*1.2, max_*1.2)        #    re-range the chart
+    plt.plot([0, 0], [-max_*1.2, max_*1.2])        #    draw the y-axis
+    plt.plot([-max_*1.2, max_*1.2], [0, 0])        #    draw the x-axis
+    
+    plt.show()
+    
+    ############    Part 1 finished.    ############
+    
+    theta = float(input("Please insert degree(s) to rotate: "))
+    deltax = float(input("Please insert the unit(s) of x-direction transform: "))
+    deltay = float(input("Please insert the unit(s) of y-direction transform: "))
+    
+    transform_matrix = npy.eye(3)       #    Identity matrix 
+    transform_matrix = transform_matrix.dot(transform(deltax, deltay))       #    transform every points by x+0, y-5.
+    transform_matrix = transform_matrix.dot(rotate(theta))             #    rotate every points about (0, 0) for 45 degrees.
+    
+    for i in range(0, 100):
+        plt.scatter(x[i], y[i], 20, 'r')
+    
+    for point in points:
+        point = transform_matrix.dot(point)
+        plt.scatter(point[0], point[1], 20, 'b')
+        if(abs(point[0]) > max_):
+            max_ = abs(point[0])
+        if(abs(point[1]) > max_):
+            max_ = abs(point[1])
+    
+    plt.xlim(-max_*1.2, max_*1.2)        #    re-range the chart
+    plt.ylim(-max_*1.2, max_*1.2)        #    re-range the chart
+    plt.plot([0, 0], [-max_*1.2, max_*1.2])        #    draw the y-axis
+    plt.plot([-max_*1.2, max_*1.2], [0, 0])        #    draw the x-axis
+    plt.show()
+    
+    ############    Part 2 finished.    ############
+    
+    print(transform_matrix)
